ee9e9de209c8fa58f108d2e5c1416e99b9441504
Houcine GASMI <houcine.gasmi@outlook.com>
2017-01-21 00:01:01 +0100

    Remove Primefaces css & redirect / to the index page.

--- Web/src/main/java/com/houcine/App.java
+++ Web/src/main/java/com/houcine/App.java
@@ -4,6 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.web.ErrorMvcAutoConfiguration;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 /**
  * Hello world!
@@ -16,6 +20,17 @@ public class App
 {
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
+    }
+
+    @Configuration
+    public class DefaultView extends WebMvcConfigurerAdapter {
+
+        @Override
+        public void addViewControllers( ViewControllerRegistry registry ) {
+            registry.addViewController( "/" ).setViewName( "forward:/index.jsf" );
+            registry.setOrder( Ordered.HIGHEST_PRECEDENCE );
+            super.addViewControllers( registry );
+        }
     }
 
 }
